328c1521d5a14a50652d02527ecc47e870a9eb31
Ginevra Di Maio <ginevra.dimaio@iismontalegenova.it>
2024-11-13 13:37:45 +0100

    ciao

---
+++ programma.js
@@ -0,0 +1,13 @@
+
+
+let foglio = document.getElementById("foglio");
+
+let disegno = foglio.getContext("2d");
+
+disegno.fillStyle = 'pink';
+
+disegno.fillRect(200,60, 50, 100);
+
+disegno.fillStyle = 'purple'
+
+disegno.fillRect(320,60, 100, 50);
